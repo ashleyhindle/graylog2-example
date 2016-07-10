40157d03254fbdd883671e3026aa049799f5089f
Ashley Hindle <ashley@smellynose.com>
2016-07-10 15:19:49 +0100

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,2 +1,2 @@
-echo "*.* @${DOMAIN}:5140;RSYSLOG_SyslogProtocol23Format" >> /etc/rsyslog.d/50-default.conf
+echo "*.* @${GRAYLOG_DOMAIN}:5140;RSYSLOG_SyslogProtocol23Format" >> /etc/rsyslog.d/50-default.conf
 service rsyslog restart
